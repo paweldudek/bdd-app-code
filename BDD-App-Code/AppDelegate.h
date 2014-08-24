b2ac498e0494adba3c4e532f842c6d696a16cba5
Pawel Dudek <pawel@dudek.mobi>
2014-08-24 13:18:14 +0200

    Initial commit

---
+++ BDD-App-Code/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BDD-App-Code
+//
+//  Created by Pawel Dudek on 24/08/14.
+//  Copyright (c) 2014 Dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BDD-App-CodeTests/BDD_App_CodeTests.m
@@ -0,0 +1,34 @@
+//
+//  BDD_App_CodeTests.m
+//  BDD-App-CodeTests
+//
+//  Created by Pawel Dudek on 24/08/14.
+//  Copyright (c) 2014 Dudek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BDD_App_CodeTests : XCTestCase
+
+@end
+
+@implementation BDD_App_CodeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
