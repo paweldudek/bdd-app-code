e3e68d898a4f0d2b4a425770d490164bd9139502
Pawel Dudek <pawel@dudek.mobi>
2014-08-25 08:08:07 +0200

    Started working on stuff

---
+++ BDD-App-Code/Sign In Service/SignInService.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2014 Taptera Inc. All rights reserved.
+ */
+
+
+#import <Foundation/Foundation.h>
+
+@protocol SignInService <NSObject>
+
+
+@end
---
+++ BDD-App-Code/Sign In Service/SignInViewController.m
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2014 Taptera Inc. All rights reserved.
+ */
+
+
+#import "SignInViewController.h"
+#import "SignInService.h"
+
+
+@implementation SignInViewController
+@end
