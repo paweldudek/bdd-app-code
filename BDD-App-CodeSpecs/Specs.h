d6493b2fd50bac632d41e93a65c070c56f58afa8
Pawel Dudek <p@taptera.com>
2014-08-27 18:26:14 +0200

    And added specs file

---
+++ BDD-App-CodeSpecs/Specs.h
@@ -0,0 +1,11 @@
+#define SPT_CEDAR_SYNTAX
+#import <Specta/Specta.h>
+
+#define EXP_SHORTHAND
+#import "Expecta.h"
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import "OCMockito.h"
